c31de5035a83f9a9dac4b017a69a429ea450f65a
Sonya <sonenkkaa@gmail.com>
2025-04-14 02:18:58 +0300

    update api.js

--- src/js/modules/api.js
+++ src/js/modules/api.js
@@ -3,22 +3,19 @@ import { state } from './state.js';
 export const POKEMONS_PER_PAGE = 12;
 
 export async function fetchAllPokemons() {
-  try {
-    const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=10000");
-    const data = await response.json();
-    
-    state.allPokemons = data.results.filter(pokemon => {
-      const id = getPokemonIDFromURL(pokemon.url);
-      return id < 10000;
-    });
-    
-    console.log("Pokemons loaded:", state.allPokemons); // Выводим загруженные покемоны
-
-    state.totalPages = Math.ceil(state.allPokemons.length / POKEMONS_PER_PAGE);
-  } catch (error) {
-    console.error("Error fetching all pokemons:", error);
-    throw error;
-  }
+    try {
+        const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=10000");
+        const data = await response.json();
+        
+        state.allPokemons = data.results.filter(pokemon => {
+            return getPokemonIDFromURL(pokemon.url) < 10000;
+        });
+        
+        state.totalPages = Math.ceil(state.allPokemons.length / POKEMONS_PER_PAGE);
+    } catch (error) {
+        console.error("Error fetching all pokemons:", error);
+        throw error;
+    }
 }
 
 export async function fetchPokemonData(id) {
@@ -32,6 +29,11 @@ export async function fetchPokemonData(id) {
 }
 
 export function getPokemonIDFromURL(url) {
-    const segments = url.split("/").filter(Boolean);
-    return parseInt(segments[segments.length - 1], 10);
+    try {
+        const segments = url.split("/").filter(Boolean);
+        return parseInt(segments[segments.length - 1], 10);
+    } catch (error) {
+      console.error(`Error parsing Pokemon URL: ${url}`, error);
+      return null;
+    }
 }
